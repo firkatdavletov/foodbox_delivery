9bc1f10d21f70355cf446da922ee9171d14a86a1
firkatdavletov <dr.firkat@ya.ru>
2025-12-29 01:09:35 +0500

    0.0.1-SNAPSHOT fix

--- src/main/kotlin/ru/foodbox/delivery/controllers/catalog/body/CreateCategoryResponseBody.kt
+++ src/main/kotlin/ru/foodbox/delivery/controllers/catalog/body/CreateCategoryResponseBody.kt
@@ -1,6 +1,6 @@
 package ru.foodbox.delivery.controllers.catalog.body
 
-import ru.foodbox.delivery.controllers.base.BaseResponseModel
+import ru.foodbox.delivery.controllers.base.ResponseModel
 import ru.foodbox.delivery.services.dto.CategoryDto
 
 class CreateCategoryResponseBody(
@@ -8,7 +8,7 @@ class CreateCategoryResponseBody(
     override val success: Boolean,
     override val error: String?,
     override val code: Int?,
-) : BaseResponseModel<CategoryDto>(category, success, error, code) {
+) : ResponseModel {
     constructor(category: CategoryDto): this(category, true, null, null)
     constructor(error: String?, code: Int?): this(null, false, error, code)
 }
